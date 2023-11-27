cea353017b2ee3ab448a957d57d6b0e567f85a97
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-27 16:57:03 -0600

    :construction: Work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Calculadora from "./components/Calculadora";
 import Themes from "./components/themes/Themes";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <main className="h-screen w-full max-w-xs mx-auto">
       <Themes />
       <Calculadora />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GitHub from "./icons/GitHub";
+import LinkedIn from "./icons/LinkedIn";
+
+const Footer = () => {
+  return (
+    <div className="flex pt-4 ">
+      <GitHub />
+      <LinkedIn />
+    </div>
+  );
+};
+
+export default Footer;
