e7458a6a550d584de80175bd6b0c8c7d55dce13b
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-28 13:04:41 -0600

    :construction: Work in progress

--- src/components/icons/LinkedIn.jsx
+++ src/components/icons/LinkedIn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LinkedIn = () => {
   return (
     <div className="translate-x-2">
-      <button class="flex gap-3 cursor-pointer m-2 text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
+      <button className="flex gap-3 cursor-pointer m-2 text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
         <svg
           viewBox="0 0 24 24"
           height="24"
